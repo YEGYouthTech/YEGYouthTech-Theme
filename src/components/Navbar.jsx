e1c46ee76412b0f564d1485583103a57ccee9c8c
Oliver Chen <o.chen1@share.epsb.ca>
2022-09-29 17:52:44 -0600

    merge & add staggered animation for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import { useAnimationState } from '../context/AnimationStateContext'
 
 export default function Navbar() {
-  const [isClicked, setIsClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState(false)
+  const { animationState } = useAnimationState()
 
   return (
-    <nav className="fixed top-[2.5rem] right-[2.5rem] z-50 w-[14rem] bg-gradient-to-br from-emerald-400 to-sky-500 px-4 py-4 rounded-md">
+    <nav
+      className="fixed top-[2.5rem] right-[2.5rem] z-50 w-[14rem] bg-gradient-to-br from-emerald-400 to-sky-500 px-4 py-4 rounded-md"
+      style={{
+        opacity: !animationState ? 0 : 1,
+        transform: !animationState
+          ? `translate3d(100%, 0, 0)`
+          : `translate3d(0, 0, 0)`,
+        transition: `opacity 500ms ease-in-out 1800ms, transform 500ms ease-in-out 1800ms`,
+      }}
+    >
+      {/* Logo */}
+
       <div className="flex flex-row items-center justify-between">
         <div>
           <a href="#">
@@ -17,16 +30,14 @@ export default function Navbar() {
         </div>
 
         <div className="w-9 h-6 flex flex-col items-center justify-between cursor-pointer">
-          {
-            ["full", "3/4", "2/4"].map((n, index) => {
-              return (
-                <span
-                  key={index}
-                  className={`w-${n} h-[3px] bg-neutral-900/75 rounded-md ml-auto`}>
-                </span>
-              );
-            })
-          }
+          {['full', '3/4', '2/4'].map((n, index) => {
+            return (
+              <span
+                key={index}
+                className={`w-${n} h-[3px] bg-neutral-900/75 rounded-md ml-auto`}
+              ></span>
+            )
+          })}
         </div>
       </div>
 
@@ -56,5 +67,5 @@ export default function Navbar() {
         </ul>
       </div>
     </nav>
-  );
+  )
 }
